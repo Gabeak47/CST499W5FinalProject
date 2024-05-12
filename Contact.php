d9362bad608fd61648a200c83025fadce69c0ac6
Gabeak47 <166718460+Gabeak47@users.noreply.github.com>
2024-05-12 11:53:52 +0900

    Add files via upload

---
+++ Contact.php
@@ -0,0 +1,62 @@
+<?php
+    include_once 'Database_connect.php';
+
+?>
+
+<div
+</div>
+<a href="Homepage.php">Home</a>
+  <div
+</div>
+    <a href="Contact.php">Contact Us</a>
+   <div
+</div>
+    <a href="Login.php">Login</a>
+<div
+</div>
+    <a href="Registration_page.php">Registration</a>
+   <div
+</div>
+    <a href="Classes.php">Classes</a>
+
+<?php if (isset($message)) : ?>
+    <div class="alert alert-success">
+        <?= $message ?>
+    </div>
+<?php endif ?>
+
+<form action="index.php" method="post">
+    <header>
+        <h1>Send Us a Message</h1>
+    </header>
+
+    <div>
+        <label for="name">Name:</label>
+        <input type="text" value="<?= $inputs['name'] ?? '' ?>" name="name" id="name" placeholder="Full name">
+        <small><?= $errors['name'] ?? '' ?></small>
+    </div>
+
+    <div>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" value="<?= $inputs['email'] ?? '' ?>" placeholder="Email address">
+        <small><?= $errors['email'] ?? '' ?></small>
+    </div>
+
+    <div>
+        <label for="subject">Subject:</label>
+        <input type="subject" name="subject" id="subject" value="<?= $inputs['subject'] ?? '' ?>" placeholder="Enter a subject">
+        <small><?= $errors['subject'] ?? '' ?></small>
+    </div>
+
+    <div>
+        <label for="message">Message:</label>
+        <textarea id="message" name="message" rows="5"><?= $inputs['message'] ?? '' ?></textarea>
+        <small><?= $errors['message'] ?? '' ?></small>
+    </div>
+
+    <label for="nickname" aria-hidden="true" class="user-cannot-see"> Nickname
+        <input type="text" name="nickname" id="nickname" class="user-cannot-see" tabindex="-1" autocomplete="off">
+    </label>
+
+    <button type="submit">Send Message</button>
+</form>
